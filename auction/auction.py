833f345bb1fc206e273d4e8ca25787c2566ef95a
binghy <binghy@users.noreply.github.com>
2022-10-10 19:04:34 +0200

    added main & test_auction

--- auction/auction.py
+++ auction/auction.py
@@ -7,15 +7,25 @@ from pyteal import *
 import os
 import json
 from algosdk.future import transaction
-from algosdk.atomic_transaction_composer import (
-    TransactionWithSigner,
-)
+from algosdk.atomic_transaction_composer import (TransactionWithSigner)
 
 class Auction(Application):
+
+    ##############
+    # Application State
+    ##############
+
+    # Declare Application state, marking `Final` here so the python class var doesn't get changed
+    # Marking a var `Final` does _not_ change anything at the AVM level
+
     # Global Bytes (2)
-    owner: Final[ApplicationStateValue] = ApplicationStateValue(
-        stack_type=TealType.bytes
+    governor: Final[ApplicationStateValue] = ApplicationStateValue(
+        stack_type = TealType.bytes,
+        key = Bytes("g"),
+        default = Global.creator_address(),
+        descr = "The current governor of this contract, allowed to do admin type actions",
     )
+
     highest_bidder: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.bytes
     )
@@ -24,22 +34,43 @@ class Auction(Application):
     auction_end: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.uint64
     )
+
     highest_bid: Final[ApplicationStateValue] = ApplicationStateValue(
         stack_type=TealType.uint64
     )
+
+    ##############
+    # Constants         <<<---- ????
+    ##############
+
+    ##############
+    # Administrative Actions
+    ##############
+
+    # Call this only on create
+
+    #@create
+    #def create(self):
+    #    return self.initialize_application_state()
 
     @create
     def create(self):
         return Seq(
             [
-                self.owner.set(Txn.sender()),
+                self.governor.set(Txn.sender()),
                 self.highest_bidder.set(Bytes("")),
                 self.highest_bid.set(Int(0)),
                 self.auction_end.set(Int(0)),
             ]
         )
 
-    @external
+    # Only the account set in app_state.owner may call this method
+    @external(authorize=Authorize.only(governor))
+    def set_governor(self, new_governor: abi.Account):
+        """sets the governor of the contract, may only be called by the current governor"""
+        return self.governor.set(new_governor.address())
+
+    @external(authorize=Authorize.only(governor))
     def start_auction(
         self,
         payment: abi.PaymentTransaction,
@@ -76,7 +107,7 @@ class Auction(Application):
     def end_auction(self):
         auction_end = self.auction_end.get()
         highest_bid = self.highest_bid.get()
-        owner = self.owner.get()
+        owner = self.governor.get()
         highest_bidder = self.highest_bidder.get()
 
         return Seq(
@@ -84,10 +115,11 @@ class Auction(Application):
             self.pay(owner, highest_bid),
             # Set global state
             self.auction_end.set(Int(0)),
-            self.owner.set(highest_bidder),
+            self.governor.set(highest_bidder),
             self.highest_bidder.set(Bytes("")),
         )
 
+#    @external(bidders=Authorize.holds_token(asset_id: Expr))
     @external
     def bid(self, payment: abi.PaymentTransaction, previous_bidder: abi.Account):
         payment = payment.get()
@@ -108,84 +140,7 @@ class Auction(Application):
             self.highest_bidder.set(payment.sender()),
         )
 
-def main():
-    # Recupero degli account da sandbox
-    accts = get_accounts()
-
-    # Prendo il primo account
-    acct1 = accts.pop()
-    acct2 = accts.pop()
-
-    # Prendo il client per interagire con la sandbox
-    client = get_algod_client()
-
-    # Creo lo smart contract
-    app = Auction()
-
-    # Metto insieme client, smart contract e account
-    app_client = ApplicationClient(client, app, signer=acct1.signer)
-
-    # Faccio il deploy dello smart contract sulla rete
-    app_id, app_address, transaction_id = app_client.create()
-
-    print(
-        f"DEPLOYED: App ID: {app_id} Address: {app_address} Transaction ID: {transaction_id}"
-    )
-
-    print(f"Current app state: {app_client.get_application_state()}")
-
-    # Preparo la transazione che invia gli algo allo smart contract
-    interaction_client = app_client.prepare(signer=acct1.signer)
-
-    # Creo i suggested params (non so cosa siano, credo sia una cosa di default)
-    sp = client.suggested_params()
-
-    # creo la transazione che manda gli algo allo smartcontract
-    tx=TransactionWithSigner(
-        txn=transaction.PaymentTxn(acct1.address, sp, app_address, 1 * consts.algo),
-        signer=acct1.signer,
-    )
-
-    # INTERAZIONE VERA E PROPRIA CON SMART CONTRACT
-    try:
-        result = interaction_client.call(
-            app.start_auction,
-            payment=tx,
-            starting_price=1*consts.algo,
-            length=1000000
-        )
-
-    except LogicException as e:
-        print(f"\n{e}\n")
-    
-
-    print(f"Current app state: {app_client.get_application_state()}")
-
-    # Preparo una nuova transazione dal secondo account per inserire una nuova puntata
-    bid_client = app_client.prepare(signer=acct2.signer)
-    tx2=TransactionWithSigner(
-        txn=transaction.PaymentTxn(acct2.address, sp, app_address, 2 * consts.algo),
-        signer=acct2.signer,
-    )
-
-    try:
-        result = bid_client.call(
-            app.bid,
-            payment=tx2,
-            previous_bidder=acct1.address
-        )
-
-    except LogicException as e:
-        print(f"\n{e}\n")
-
-    print(f"Current app state: {app_client.get_application_state()}")
-
-    try:
-        app_client1.close_out()
-        app_client2.close_out()
-        app_client1.delete()
-    except Exception as e:
-        print(e)
 
 if __name__ == "__main__":
-    main()
+#    main()
+    Auction().dump("artifacts")
---
+++ auction/test_auction.py
@@ -0,0 +1,132 @@
+import copy
+
+import pytest
+import typing
+
+from algosdk.atomic_transaction_composer import (
+    AtomicTransactionComposer,
+    TransactionWithSigner,
+    AccountTransactionSigner,
+)
+from algosdk.future import transaction
+from algosdk.v2client.algod import AlgodClient
+from algosdk.encoding import decode_address
+from beaker import client, sandbox, testing
+from beaker.client.application_client import ApplicationClient
+from beaker.sandbox import get_algod_client, get_accounts
+from beaker.client.logic_error import LogicException
+from .auction import Auction
+
+accts = get_accounts()
+client = get_algod_client()
+
+
+@pytest.fixture(scope="session")
+def creator_acct() -> tuple[str, str, AccountTransactionSigner]:
+    return accts[0].address, accts[0].private_key, accts[0].signer
+
+
+@pytest.fixture(scope="session")
+def user_acct() -> tuple[str, str, AccountTransactionSigner]:
+    return accts[1].address, accts[1].private_key, accts[1].signer          #for loop for many users?
+
+
+@pytest.fixture(scope="session")
+def creator_app_client(creator_acct) -> client.ApplicationClient:
+    _, _, signer = creator_acct
+    app = Auction()
+    app_client = client.ApplicationClient(client, app, signer=signer)
+    return app_client
+
+
+def test_app_create(creator_app_client: client.ApplicationClient):
+    creator_app_client.create()
+    app_state = creator_app_client.get_application_state()
+    sender = creator_app_client.get_sender()
+
+    assert (
+        app_state[Auction.governor.str_key()] == decode_address(sender).hex()
+    ), "The governor should be my address"
+
+
+
+#def minimum_fee_for_txn_count(sp: transaction.SuggestedParams, txn_count: int) -> transaction.SuggestedParams:
+#    """
+#    Configures transaction fee _without_ considering network congestion.
+#    Since the function does not account for network congestion, do _not_ use the function as-is in a production use-case.
+#    """
+#    s = copy.deepcopy(sp)
+#    s.flat_fee = True
+#    s.fee = transaction.constants.min_txn_fee * txn_count
+#    return s
+
+#def assert_app_algo_balance(c: client.ApplicationClient, expected_algos: int):
+#    """
+#    Verifies the app's algo balance is not unexpectedly drained during app interaction (e.g. paying inner transaction fees).
+#    Due to the presence of rewards, the assertion tolerates actual > expected for small positive differences.
+#    """
+#    xs = testing.get_balances(c.client, [c.app_addr])
+#    assert c.app_addr in xs
+#    assert 0 in xs[c.app_addr]
+#    actual_algos = xs[c.app_addr][0]
+
+#    # Before accounting for rewards, confirm algos were not drained.
+#    assert actual_algos >= expected_algos
+
+#    # Account for rewards.
+#    micro_algos_tolerance = 10
+#    assert actual_algos - expected_algos <= micro_algos_tolerance
+
+
+#app_algo_balance: typing.Final = int(1e7)
+
+
+
+#def test_app_fund(creator_app_client: ApplicationClient):
+#    app_addr, addr, signer = (
+#        creator_app_client.app_addr,
+#        creator_app_client.sender,
+#        creator_app_client.signer,
+#    )
+
+#    pool_asset, a_asset, b_asset = _get_tokens_from_state(creator_app_client)
+
+#    assert addr
+#    _opt_in_to_token(addr, signer, pool_asset)
+
+#    balance_accts = [app_addr, addr]
+#    balances_before = testing.get_balances(creator_app_client.client, balance_accts)
+
+#    a_amount = 10000
+#    b_amount = 3000
+
+#    sp = creator_app_client.client.suggested_params()
+#    creator_app_client.call(
+#        ConstantProductAMM.mint,
+#        suggested_params=minimum_fee_for_txn_count(sp, 2),
+#        a_xfer=TransactionWithSigner(
+#            txn=transaction.AssetTransferTxn(addr, sp, app_addr, a_amount, a_asset),
+#            signer=signer,
+#        ),
+#        b_xfer=TransactionWithSigner(
+#            txn=transaction.AssetTransferTxn(addr, sp, app_addr, b_amount, b_asset),
+#            signer=signer,
+#        ),
+#        pool_asset=pool_asset,
+#        a_asset=a_asset,
+#        b_asset=b_asset,
+#    )
+
+#    balances_after = testing.get_balances(creator_app_client.client, balance_accts)
+#    balance_deltas = testing.get_deltas(balances_before, balances_after)
+
+#    assert balance_deltas[app_addr][a_asset] == a_amount
+#    assert balance_deltas[app_addr][b_asset] == b_amount
+#    assert_app_algo_balance(creator_app_client, app_algo_balance)
+
+#    expected_pool_tokens = int((a_amount * b_amount) ** 0.5 - ConstantProductAMM._scale)
+#    assert balance_deltas[addr][pool_asset] == expected_pool_tokens
+
+#    ratio = _get_ratio_from_state(creator_app_client)
+#    expected_ratio = int((a_amount * ConstantProductAMM._scale) / b_amount)
+#    assert ratio == expected_ratio
